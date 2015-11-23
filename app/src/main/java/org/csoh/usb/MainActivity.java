3e8c4dcc71cf58c378e721cbd3335a3d05b1f7d3
Calvin <soh_calvin@yahoo.com>
2015-11-23 22:03:49 +0800

    Receiving data from accessory

--- app/src/main/java/org/csoh/usb/MainActivity.java
+++ app/src/main/java/org/csoh/usb/MainActivity.java
@@ -22,6 +22,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements Runnable {
+    private final String logName = "MainActivity";
     private FileInputStream mInputStream;
     private FileOutputStream mOutputStream;
     //UsbAccessory mAccessory;
@@ -42,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements Runnable {
                         .setAction("Action", null).show();
             }
         });
+        Thread thread = new Thread(null, this, "AccessoryThread");
+        Toast.makeText(MainActivity.this, "Starting thread", Toast.LENGTH_SHORT).show();
+        thread.start();
     }
 
     @Override
@@ -66,13 +70,87 @@ public class MainActivity extends AppCompatActivity implements Runnable {
         return super.onOptionsItemSelected(item);
     }
 
-    public void sendMessage(View view) {
-        EditText editText = (EditText)findViewById(R.id.editText);
-        Toast.makeText(MainActivity.this, editText.getText(), Toast.LENGTH_SHORT).show();
-        readMessageFromAccessory();
+    private boolean openAndSetupAccessoryIfNecessary() {
+        if(mFileDescriptor == null) {
+            UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            UsbAccessory accessory = (UsbAccessory) getIntent().getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+            if (accessory == null) {
+                Log.d(logName, "Accessory not found.");
+                return false;
+            }
+            mFileDescriptor = mUsbManager.openAccessory(accessory);
+
+
+        }
+        return true;
     }
+
+    private FileOutputStream makeOutputStream(){
+        openAndSetupAccessoryIfNecessary();
+        if (mFileDescriptor != null) {
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            return new FileOutputStream(fd);
+        }else{
+            Log.d(logName,"Unable to makeOutputStream as mFileDescriptor is null");
+            return null;
+        }
+    }
+    private FileInputStream makeInputStream(){
+        openAndSetupAccessoryIfNecessary();
+        if (mFileDescriptor != null) {
+            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
+            Log.d(logName,"Call to makeInputStream");
+            return new FileInputStream(fd);
+        }else{
+            Log.d(logName,"Unable to makeInputStream as mFileDescriptor is null");
+            return null;
+        }
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = (EditText)findViewById(R.id.textboxMessageToHost);
+        FileOutputStream out = makeOutputStream();
+        if(out == null){
+            Toast.makeText(MainActivity.this, "Error : outputStream is null", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String messToSend = editText.getText().toString();
+        try {
+            out.write(messToSend.getBytes());
+            Toast.makeText(MainActivity.this, "Sent : " + messToSend, Toast.LENGTH_SHORT).show();
+            out.close();
+        } catch (IOException e) {
+            Toast.makeText(MainActivity.this, "Error sending message : " + messToSend + " : " +e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
     private void readMessageFromAccessory() {
-        UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        FileInputStream mInputStream = null;
+        while(mInputStream == null) {
+            mInputStream = makeInputStream();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+        if(mInputStream != null){
+            while(true){
+                try {
+                    byte[] buffer = new byte[32];
+                    mInputStream.read(buffer);
+                    Log.d(logName,">>> " + new String(buffer));
+                } catch (IOException e) {
+                    Log.d(logName, ">>> " + e.toString());
+                }
+            }
+        }else{
+            Log.d(logName, "Error makeInputStream() returned null");
+            return;
+        }
+   /*     UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         UsbAccessory accessory = (UsbAccessory) getIntent().getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
         Log.d("MainAct", "openAccessory: " + accessory);
         if(accessory == null){
@@ -84,15 +162,18 @@ public class MainActivity extends AppCompatActivity implements Runnable {
         if (mFileDescriptor != null) {
             FileDescriptor fd = mFileDescriptor.getFileDescriptor();
             mInputStream = new FileInputStream(fd);
-            mOutputStream = new FileOutputStream(fd);
             Thread thread = new Thread(null, this, "AccessoryThread");
             Toast.makeText(MainActivity.this, "Starting thread" + mFileDescriptor, Toast.LENGTH_SHORT).show();
             thread.start();
         }
+        */
     }
     @Override
     public void run() {
-        int loop = 0;
+        Log.d(logName, "Before read");
+        readMessageFromAccessory();
+        Log.d(logName,"After read");
+        /*int loop = 0;
         Log.d("MainAct", ">>> in run()");
         while(loop++ < 20) {
             Log.d("MainAct",">>> Loop " + loop);
@@ -112,6 +193,7 @@ public class MainActivity extends AppCompatActivity implements Runnable {
             }
 
         }
+        */
     }
 
 }
